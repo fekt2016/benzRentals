813fba976a1a1e177ddccecfe292008d7053f7d7
yussif <fekt2016@icloud.com>
2025-09-16 14:01:34 +0000

    images

--- src/components/CarShowcase.jsx
+++ src/components/CarShowcase.jsx
@@ -5,19 +5,38 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { getPublicUrl } from "../utils/publicUrl";
 
 const cars = [
-  { name: "Mercedes S-Class", img: "images/s-class.jpg", price: "$250/day" },
-  { name: "Mercedes G-Wagon", img: "images/g-wagon.jpg", price: "$400/day" },
-  { name: "Mercedes C-Class", img: "images/c-class.jpg", price: "$180/day" },
-  { name: "Mercedes E-Class", img: "images/e-class.jpg", price: "$220/day" },
-  { name: "Mercedes AMG GT", img: "images/amg-gt.jpg", price: "$500/day" },
+  {
+    name: "Mercedes-Benz S-Class",
+    img: "/images/Class31.webp",
+    price: "$250/day",
+  },
+  {
+    name: "Mercedes-Benz G-Wagon",
+    img: "/images/Class2.avif",
+    price: "$400/day",
+  },
+  {
+    name: "Mercedes-Benz C-Class",
+    img: "/images/Class1a.jpg",
+    price: "$180/day",
+  },
+  {
+    name: "Mercedes-Benz E-Class",
+    img: "/images/2025-mercedes-benz-gle-350.avif",
+    price: "$220/day",
+  },
+  {
+    name: "Mercedes-Benz AMG GT",
+    img: "/images/Class1.avif",
+    price: "$500/day",
+  },
 ];
 
 const CarShowcase = () => {
   return (
-    <Wrapper>
+    <ShowcaseWrapper>
       <h2>Our Featured Models</h2>
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
@@ -32,34 +51,34 @@ const CarShowcase = () => {
           1024: { slidesPerView: 3 },
         }}
       >
-        {cars.map((car, i) => {
-          const imgUrl = getPublicUrl(car.img);
-          return (
-            <SwiperSlide key={i}>
-              <Card>
-                <img src={imgUrl} alt={car.name} />
-                <h3>{car.name}</h3>
-                <p>{car.price}</p>
-              </Card>
-            </SwiperSlide>
-          );
-        })}
+        {cars.map((car, i) => (
+          <SwiperSlide key={i}>
+            <CarCard>
+              <img src={car.img} alt={car.name} />
+              <h3>{car.name}</h3>
+              <p>{car.price}</p>
+            </CarCard>
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </Wrapper>
+    </ShowcaseWrapper>
   );
 };
 
 export default CarShowcase;
 
-const Wrapper = styled.section`
+const ShowcaseWrapper = styled.section`
   padding: 4rem 2rem;
-  background: ${({ theme }) => theme.colors.background};
   text-align: center;
 
   h2 {
     font-size: 2.2rem;
     margin-bottom: 2rem;
     color: ${({ theme }) => theme.colors.primary};
+  }
+
+  .swiper {
+    padding-bottom: 3rem;
   }
 
   .swiper-button-prev,
@@ -72,9 +91,9 @@ const Wrapper = styled.section`
   }
 `;
 
-const Card = styled.div`
+const CarCard = styled.div`
   background: #fff;
-  border-radius: 12px;
+  border-radius: ${({ theme }) => theme.radius.medium};
   overflow: hidden;
   box-shadow: ${({ theme }) => theme.shadows.card};
   transition: transform 0.3s;
